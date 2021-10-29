3d3f485d5b235d8f7f748abebf27887e83210e4f
望月祐汰 <mczk9402@gmail.com>
2021-10-29 17:34:41 +0900

    ♻️ メイン部分のコンポーネント化

---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Home.module.css";
+import { Headline } from "../components/Headline";
+import { Links } from "../components/Links";
+
+export function Main(props) {
+  return (
+    <main className={styles.main}>
+      <Headline page={props.page}>
+        <code className={styles.code}>pages/{props.page}.js</code>
+      </Headline>
+
+      <Links />
+    </main>
+  );
+}

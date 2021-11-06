397f05d83ae7abcca66981fc274852fd7dfaf560
望月祐汰 <mczk9402@gmail.com>
2021-11-07 00:45:29 +0900

    ♻️ リファクタリング

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 import classes from "src/components/Footer/Footer.module.css";
 
-export function Footer() {
+export const Footer = () => {
   return (
     <footer className={classes.footer}>
       <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
@@ -8,4 +8,4 @@ export function Footer() {
       </a>
     </footer>
   );
-}
+};
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import { Footer } from "src/components/Footer";
 
-export default function About({ doubleCount, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd }) {
+const About = (props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,14 +13,14 @@ export default function About({ doubleCount, isShow, handleClick, handleDisplay,
 
       <Header />
       <div className={styles["counter-wrap"]}>
-        {isShow ? <h1>{doubleCount}</h1> : null}
-        <button onClick={handleClick}>ボタン</button>
-        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+        {props.isShow ? <h1>{props.doubleCount}</h1> : null}
+        <button onClick={props.handleClick}>ボタン</button>
+        <button onClick={props.handleDisplay}>{props.isShow ? "非表示" : "表示"}</button>
 
-        <input type="text" value={text} onChange={handleChange} />
-        <button onClick={handleAdd}>追加</button>
+        <input type="text" value={props.text} onChange={props.handleChange} />
+        <button onClick={props.handleAdd}>追加</button>
         <ul>
-          {array.map((item) => {
+          {props.array.map((item) => {
             return <li key={item}>{item}</li>;
           })}
         </ul>
@@ -30,4 +30,6 @@ export default function About({ doubleCount, isShow, handleClick, handleDisplay,
       <Footer />
     </div>
   );
-}
+};
+
+export default About;

3203f3ddccdd8b6c5026c7970b5ab6d1bd63a8da
望月祐汰 <mczk9402@gmail.com>
2021-12-18 12:51:54 +0900

    🐛  eslintの修正

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,14 @@
-import classes from "src/components/Footer/Footer.module.css";
+import classes from 'src/components/Footer/Footer.module.css';
 
 export const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         Powered by <img src="/vercel.svg" alt="Vercel Logo" className={classes.logo}></img>
       </a>
     </footer>
